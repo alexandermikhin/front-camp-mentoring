bdd59e0a3b4e2daaff84f5af5405e7d6817cff87
Aliaksandr Mikhin <Aliaksandr_Mikhin@epam.com>
2019-10-25 16:14:47 +0300

    feat (task1): add webpack-dev-server
    Add webpack-dev-server. Add source-maps.

--- task1/webpack.config.js
+++ task1/webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
-  plugins: [new CleanWebpackPlugin()],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    port: 4200
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+    //   title: 'Task 1 - ES7 + BABEL + WEBPACK',
+      template: 'index.html'
+    })
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
