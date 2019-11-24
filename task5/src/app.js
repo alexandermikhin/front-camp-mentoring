518cb4df72b16d5a9da6fac9094909b0d0c52fb8
Aliaksandr Mikhin <Aliaksandr_Mikhin@epam.com>
2019-11-24 18:35:43 +0300

    feat (task5): implement CRUD operation in application
    Implement CRUD operations in application.

--- task5/src/app.js
+++ task5/src/app.js
@@ -1,9 +1,48 @@
 const express = require("express");
-const news = require("./data/news.json");
+const data = require("./data/news.json");
 const app = express();
 
+app.get("/news", (_req, res) => {
+  res.send(data.news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const newsItem = data.news.find(newsItem => newsItem.id === id);
+  if (newsItem) {
+    res.send(newsItem);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.use(express.json());
+
+app.post("/news", (req, res) => {
+  const body = req.body;
+  const maxIdItem = data.news.reduce((acc, val) => acc.id > val.id ? acc : val);
+  const newItem = {
+    id: maxIdItem.id + 1,
+    date: body.date,
+    content: body.content
+  };
+  
+  data.news.push(newItem);
+  res.status(201).send();
+});
+
+app.delete("/news/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = data.news.findIndex(newsItem => newsItem.id === id);
+  if (index !== -1) {
+    data.news.splice(index, 1);
+  }
+
+  res.status(200).send();
+});
+
 app.all("*", (_req, res) => {
-  res.send(news);
+  res.send(data);
 });
 
 app.use((err, _req, res, _next) => {
