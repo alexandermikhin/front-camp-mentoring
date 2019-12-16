e232e08354789795477be9f49a4d444a5c7345f8
Aliaksandr Mikhin <Aliaksandr_Mikhin@epam.com>
2019-12-17 01:37:46 +0300

    feat (task8): style page not found component

--- task7/movies/src/components/page-not-found/PageNotFound.jsx
+++ task7/movies/src/components/page-not-found/PageNotFound.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import "./PageNotFound.css";
 
 export const PageNotFound = () => (
-    <div>Page not found.</div>
+    <main className="page-not-found">404. Page not found</main>
 )
