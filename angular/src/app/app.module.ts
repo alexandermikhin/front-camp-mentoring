94b6a5c9973be55d653b31c0dc1c5fcc3ffec2c5
Aliaksandr Mikhin <aliaksandr_mikhin@epam.com>
2020-01-12 12:21:35 +0300

    feat (angular): add footer, header, main view, sidebar components
    - Add Header, Footer, MainView, Sidebar components.
    - Change prefix to "fcm".

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { MainViewComponent } from "./components/main-view/main-view.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainViewComponent,
+    SidebarComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ angular/src/app/components/main-view/main-view.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "fcm-main-view",
+  templateUrl: "./main-view.component.html"
+})
+export class MainViewComponent {}
