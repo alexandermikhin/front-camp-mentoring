fc052c50355214209e257670ad3be6c733bc74d5
Aliaksandr Mikhin <aliaksandr_mikhin@epam.com>
2020-01-12 13:18:35 +0300

    feat (angular): add base application routing
    Add base application routing.

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { MainViewComponent } from "./components/main-view/main-view.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: MainViewComponent },
+  { path: "404", component: PageNotFoundComponent },
+  { path: "**", redirectTo: "/404" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
