45fb03e8b98b00b84824d3e620c170e559ba623e
Aliaksandr Mikhin <aliaksandr_mikhin@epam.com>
2020-01-12 14:15:13 +0300

    feat (angular): add routing for main and contact child routes
    Add routing for main and contact child routes.

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ContactComponent } from "./components/contact/contact.component";
 import { MainViewComponent } from "./components/main-view/main-view.component";
+import { MainComponent } from "./components/main/main.component";
 import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 
 const routes: Routes = [
-  { path: "", component: MainViewComponent },
+  {
+    path: "",
+    component: MainViewComponent,
+    children: [
+      { path: "", component: MainComponent },
+      { path: "contact", component: ContactComponent }
+    ]
+  },
   { path: "404", component: PageNotFoundComponent },
   { path: "**", redirectTo: "/404" }
 ];
