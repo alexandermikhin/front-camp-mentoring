304a62462209db8315647cf85a32bf6ac03db297
Aliaksandr Mikhin <aliaksandr_mikhin@epam.com>
2020-01-12 15:49:29 +0300

    feat (angular): add styling to the sidebar component
    Add styling to the sidebar component.

--- angular/src/app/components/sidebar/sidebar.component.ts
+++ angular/src/app/components/sidebar/sidebar.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "fcm-sidebar",
-  templateUrl: "./sidebar.component.html"
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"]
 })
 export class SidebarComponent {}
