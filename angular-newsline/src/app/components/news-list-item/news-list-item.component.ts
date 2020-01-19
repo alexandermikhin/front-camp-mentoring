ee63c0ee42ffdca33193714a67b9e8f5ee648b40
Alexander Mikhin <alexmikh@tut.by>
2020-01-19 20:15:02 +0300

    feat (angular-newsline): add styling to news list item component
    Add styling to news list item component.

--- angular-newsline/src/app/components/news-list-item/news-list-item.component.ts
+++ angular-newsline/src/app/components/news-list-item/news-list-item.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'nl-news-list-item',
-    templateUrl: './news-list-item.component.html'
+    templateUrl: './news-list-item.component.html',
+    styleUrls: ['./news-list-item.component.scss']
 })
 export class NewsListItemComponent { }
