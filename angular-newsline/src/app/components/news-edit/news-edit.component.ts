5ee5a409e27bcc980eb868537657b3d0d205e735
Alexander Mikhin <alexmikh@tut.by>
2020-01-19 21:47:19 +0300

    feat (angular-newsline): style news edit component
    Style news edit component.

--- angular-newsline/src/app/components/news-edit/news-edit.component.ts
+++ angular-newsline/src/app/components/news-edit/news-edit.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'nl-news-edit',
-    templateUrl: './news-edit.component.html'
+    templateUrl: './news-edit.component.html',
+    styleUrls: ['./news-edit.component.scss']
 })
 export class NewsEditComponent { }
