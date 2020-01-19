fe32f9f988ee48bdcb7ee15084167b03f987676d
Alexander Mikhin <alexmikh@tut.by>
2020-01-19 20:41:54 +0300

    feat (angular-newsline): style news details component
    Style news details component.

--- angular-newsline/src/app/components/news-details/news-details.component.ts
+++ angular-newsline/src/app/components/news-details/news-details.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'nl-news-details',
-    templateUrl: './news-details.component.html'
+    templateUrl: './news-details.component.html',
+    styleUrls: ['./news-details.component.scss']
 })
 export class NewsDetailsComponent { }
