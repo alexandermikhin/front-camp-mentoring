fb9d6785aacd96a114e7db674b5b97a363f67ff7
Alexander Mikhin <alexmikh@tut.by>
2020-01-19 15:51:03 +0300

    feat (angular-newsline): add markup to the toolbar component
    Add markup to the toolbar component.

--- angular-newsline/src/app/components/toolbar/toolbar.component.ts
+++ angular-newsline/src/app/components/toolbar/toolbar.component.ts
@@ -4,4 +4,6 @@ import { Component } from '@angular/core';
     selector: 'nl-toolbar',
     templateUrl: './toolbar.component.html'
 })
-export class ToolbarComponent { }
+export class ToolbarComponent {
+    sources = [ 'All', 'BBC', 'CNN'];
+}
