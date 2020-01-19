a8989f4318f5e982b72a78b50797d613188ddb82
Alexander Mikhin <alexmikh@tut.by>
2020-01-19 19:04:26 +0300

    feat (angular-newsline): add styling to toolbar component
    Add styling to toolbar component.

--- angular-newsline/src/app/components/toolbar/toolbar.component.ts
+++ angular-newsline/src/app/components/toolbar/toolbar.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'nl-toolbar',
-    templateUrl: './toolbar.component.html'
+    templateUrl: './toolbar.component.html',
+    styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent {
     sources = [ 'All', 'BBC', 'CNN'];
