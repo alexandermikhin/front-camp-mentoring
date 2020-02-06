5486c5328f3ad6bb77cfe9db2389843cef2cd9a6
Aliaksandr Mikhin <aliaksandr_mikhin@epam.com>
2020-02-06 21:29:38 +0300

    test (angular-newsline): add tests for news list item component
    Add tests for news list item component methods.

--- angular-newsline/src/app/components/news-list-item/news-list-item.component.spec.ts
+++ angular-newsline/src/app/components/news-list-item/news-list-item.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { NewsItemModel } from 'src/app/models';
 import { HighlightPipe } from 'src/app/pipes';
 import { NewsListItemComponent } from './news-list-item.component';
 
@@ -23,4 +25,57 @@ describe('NewsListItemComponent', () => {
         fixture.detectChanges();
         expect(component).toBeTruthy();
     });
+
+    it('should emit news item id on edit news click', () => {
+        spyOn(component.editNews, 'emit');
+        component.model = {
+            ...createModel(),
+            isEditable: true
+        };
+
+        fixture.detectChanges();
+        const editButtonDebugEl = fixture.debugElement.query(
+            By.css('.nl-news-list-item-operations__button')
+        );
+        editButtonDebugEl.triggerEventHandler('click', null);
+        expect(component.editNews.emit).toHaveBeenCalledWith('news-id');
+    });
+
+    it('should emit news item id on edit news click', () => {
+        spyOn(component.deleteNews, 'emit');
+        component.model = {
+            ...createModel(),
+            isEditable: true
+        };
+
+        fixture.detectChanges();
+        const deleteButtonDebugEl = fixture.debugElement.queryAll(
+            By.css('.nl-news-list-item-operations__button')
+        );
+        deleteButtonDebugEl[1].triggerEventHandler('click', null);
+        expect(component.deleteNews.emit).toHaveBeenCalledWith('news-id');
+    });
+
+    it('should emit news item on expand news click', () => {
+        spyOn(component.expandNews, 'emit');
+        component.model = createModel();
+
+        fixture.detectChanges();
+        const expandButtonDebugEl = fixture.debugElement.query(
+            By.css('.nl-button__expand')
+        );
+        expandButtonDebugEl.triggerEventHandler('click', null);
+        expect(component.expandNews.emit).toHaveBeenCalledWith(component.model);
+    });
+
+    function createModel(): NewsItemModel {
+        return {
+            id: 'news-id',
+            heading: 'news-heading',
+            shortDescription: 'news-short-description',
+            content: 'news-content',
+            date: new Date('2020-06-02'),
+            source: 'news-source'
+        };
+    }
 });
