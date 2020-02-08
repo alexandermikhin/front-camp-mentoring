297bbc509f45074dfd16e7aa648351c50a1feb9d
Alexander Mikhin <alexmikh@tut.by>
2020-02-08 12:45:12 +0300

    test (angular-newsline): add tests for logged in user name and header in header component

--- angular-newsline/src/app/components/header/header.component.spec.ts
+++ angular-newsline/src/app/components/header/header.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { of, Subject } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { Observable, of, Subject } from 'rxjs';
+import { User } from 'src/app/models';
 import { HeaderService, LoginService, UserService } from 'src/app/services';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
     let component: HeaderComponent;
     let fixture: ComponentFixture<HeaderComponent>;
-    let userService: Partial<UserService>;
+    let userService: {
+        activeUser: Subject<User>;
+        logout: () => Observable<null>;
+    };
     let loginService: jasmine.SpyObj<LoginService>;
-    let headerService: Partial<HeaderService>;
+    let headerService: {
+        activeHeader: Subject<string>;
+    };
 
     beforeEach(() => {
         userService = {
@@ -42,5 +49,31 @@ describe('HeaderComponent', () => {
     it('should create component', () => {
         fixture.detectChanges();
         expect(component).toBeDefined();
+    });
+
+    it('should display logged in user name', () => {
+        fixture.detectChanges();
+        const user: User = { login: 'user-name' };
+        userService.activeUser.next(user);
+        fixture.detectChanges();
+        const loginDebugEl = fixture.debugElement.query(
+            By.css('.nl-top-bar__login')
+        );
+
+        const loginElement: HTMLSpanElement = loginDebugEl.nativeElement;
+        expect(loginElement.innerText).toEqual('user-name');
+    });
+
+    it('should display active header', () => {
+        fixture.detectChanges();
+        const header = 'app-header';
+        headerService.activeHeader.next(header);
+        fixture.detectChanges();
+        const headerDebugEl = fixture.debugElement.query(
+            By.css('.nl-header__header')
+        );
+
+        const headerElement: HTMLSpanElement = headerDebugEl.nativeElement;
+        expect(headerElement.innerText).toEqual(header);
     });
 });
